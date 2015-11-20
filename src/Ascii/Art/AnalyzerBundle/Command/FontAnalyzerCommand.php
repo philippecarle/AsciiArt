c46df8451e6a4999c9c11a068a1c7822903b9cc5
p.carle <p.carle@lacentralepharma.com>
2015-11-20 17:38:50 +0100

    up

--- src/Ascii/Art/AnalyzerBundle/Command/FontAnalyzerCommand.php
+++ src/Ascii/Art/AnalyzerBundle/Command/FontAnalyzerCommand.php
@@ -2,9 +2,11 @@
 
 namespace Ascii\Art\AnalyzerBundle\Command;
 
+use Intervention\Image\AbstractFont;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Intervention\Image\ImageManager;
 
 class FontAnalyzerCommand extends ContainerAwareCommand
 {
@@ -27,6 +29,31 @@ class FontAnalyzerCommand extends ContainerAwareCommand
         for($i=33; $i<126; $i++) {
             $chars[] = chr((string)$i);
         }
-        var_dump($chars);
+
+        $kernel = $this->getContainer()->get('kernel');
+        $fontFile = $kernel->locateResource('@AsciiArtAnalyzerBundle/Resources/fonts/courier.ttf');
+        foreach ($chars as $char) {
+            $manager = new ImageManager(['driver' => 'imagick']);
+            $image = $manager->make(imagecreatetruecolor(200,200));
+            $image->fill('#FFFFFF');
+            $image->text($char, 100, 190, function(AbstractFont $font) use ($fontFile) {
+                $font->file($fontFile);
+                $font->size(240);
+                $font->align('center');
+            });
+            $image->save("./files/$char.jpg");
+
+            $colors = 0;
+            for($x=1; $x<= 200; $x++) {
+                for($y=1; $y<= 200; $y++) {
+                    $imagick = $image->getCore();
+                    echo $imagick->getImagePixelColor($x, $y);
+                }
+            }
+
+
+        }
+
+
     }
 }
